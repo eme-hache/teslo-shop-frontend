ea55193b385f271acb45fc02c4ab1493ccd9df02
Eme Hache <luisalfonsodsg@outlook.com>
2023-09-18 22:59:58 -0400

    fix: add await to avoid app crash :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:teslo_shop/config/config.dart';
 
 void main() async {
-  Environment.initEnvironment();
+  await Environment.initEnvironment();
 
   runApp(const ProviderScope(child: MainApp()));
 }
